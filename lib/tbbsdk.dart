8b60e79ed8e648bc38e7f2b84479ee86a9039e44
bellbeesdev <thebellbeesdev@gmail.com>
2021-01-27 17:03:26 +0530

    initial

--- lib/tbbsdk.dart
+++ lib/tbbsdk.dart
@@ -189,10 +189,10 @@ class TBBSdk {
     //  response
     if (_response.statusCode >= 200 && _response.statusCode < 300) {
       TBBResponse response = TBBResponse.fromJson(json.decode(_response.body));
-
+      print(response.toJson());
       // Setting Waiting for otp
       if (response.data != null && response.data.phone != null) {
-        _localDatabaseService.putLocalState('otp_from', response.data.phone);
+        _localDatabaseService.putLocalState('otp_from', '0');
       }
 
       return TBBAccessToken.fromJson(json.decode(response.data));
