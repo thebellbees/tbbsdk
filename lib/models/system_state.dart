f183e0f5b91563f43dac8215e985dfb4aa45ff0f
bellbeesdev <thebellbeesdev@gmail.com>
2021-02-04 18:53:28 +0530

    initial

--- lib/models/system_state.dart
+++ lib/models/system_state.dart
@@ -6,26 +6,22 @@ class TBBLocalState {
   String accessId;
   String refreshId;
   TBBUser user;
-  Map<String, TBBServiceTaxonomy> serviceTaxonomies;
+  TBBServiceTaxonomy serviceTaxonomy;
 
   bool get loggedIn => user != null && user.isEmpty == false;
 
   Future<String> get otpFrom async => await _getProp("otp_from");
 
   TBBLocalState(
-      {this.accessId, this.refreshId, this.user, this.serviceTaxonomies});
+      {this.accessId, this.refreshId, this.user, this.serviceTaxonomy});
 
   TBBLocalState.fromJson(Map<String, dynamic> json) {
     accessId = json['access_id'];
     refreshId = json['refresh_id'];
     user = json['user'] != null ? TBBUser.fromJson(json['user']) : null;
-
-    if (json["service_taxonomies"] != null) {
-      var _data = json["service_taxonomies"];
-      serviceTaxonomies["category"] = _data["category"] != null
-          ? TBBServiceTaxonomy.fromJson(_data["category"])
-          : null;
-    }
+    serviceTaxonomy = json['service_taxonomy'] != null
+        ? TBBServiceTaxonomy.fromJson(json['service_taxonomy'])
+        : null;
   }
 
   Map<String, dynamic> toJson() {
@@ -34,13 +30,8 @@ class TBBLocalState {
     data['loggedIn'] = this.loggedIn;
     data['refresh_id'] = this.refreshId;
     data['user'] = this.user != null ? this.user.toJson() : null;
-
-    if (this.serviceTaxonomies != null) {
-      var _data = this.serviceTaxonomies;
-      data["service_taxonomies"]["category"] = _data["category"] != null
-          ? (data["category"] as TBBServiceTaxonomy).toJson()
-          : null;
-    }
+    data['service_taxonomy'] =
+        this.serviceTaxonomy != null ? this.serviceTaxonomy.toJson() : null;
 
     return data;
   }
