dd41f9fb2136a63b982bd6dcac69eda1e08b1c5f
bellbeesdev <thebellbeesdev@gmail.com>
2021-02-06 13:44:06 +0530

    initial

--- lib/tbbsdk.dart
+++ lib/tbbsdk.dart
@@ -481,10 +481,10 @@ class TBBSdk {
     //  response
     if (_response.statusCode >= 200 && _response.statusCode < 300) {
       TBBResponse response = TBBResponse.fromJson(json.decode(_response.body));
-      print( response.data);
+      print(response.data);
       return response.data
           .map((service) => TBBServiceItem.fromJson(service))
-          .toList();
+          .toList() as List<TBBServiceItem>;
     } else {
       throw new TBBError.fromJson(json.decode(_response.body));
     }
