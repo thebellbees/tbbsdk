d5b94dc59cc355e9a5b87c81dbd7c7a49975f0bd
bellbeesdev <thebellbeesdev@gmail.com>
2021-04-01 20:02:24 +0530

    initial

--- lib/models/TBBStoreSubscription.g.dart
+++ lib/models/TBBStoreSubscription.g.dart
@@ -8,10 +8,16 @@ part of 'TBBStoreSubscription.dart';
 
 TBBStoreSubscription _$TBBStoreSubscriptionFromJson(Map<String, dynamic> json) {
   return TBBStoreSubscription(
+    id: json['id'] as String,
     subscriptionId: json['subscription_id'] as String,
+    transactionId: json['transaction_id'] as String,
     variantId: json['variant_id'] as String,
     storeId: json['store_id'] as String,
-    dateOfPurchase: json['date_of_purchase'] as String,
+    beginDate: json['begin_date'] as int,
+    endDate: json['end_date'] as int,
+    duration: json['duration'] as int,
+    price: (json['price'] as num)?.toDouble(),
+    status: json['status'] as String,
     createdAt: json['created_at'] as String,
     updatedAt: json['updated_at'] as String,
     deletedAt: json['deleted_at'] as String,
@@ -21,10 +27,16 @@ TBBStoreSubscription _$TBBStoreSubscriptionFromJson(Map<String, dynamic> json) {
 Map<String, dynamic> _$TBBStoreSubscriptionToJson(
         TBBStoreSubscription instance) =>
     <String, dynamic>{
+      'id': instance.id,
       'subscription_id': instance.subscriptionId,
+      'transaction_id': instance.transactionId,
       'variant_id': instance.variantId,
       'store_id': instance.storeId,
-      'date_of_purchase': instance.dateOfPurchase,
+      'duration': instance.duration,
+      'begin_date': instance.beginDate,
+      'end_date': instance.endDate,
+      'price': instance.price,
+      'status': instance.status,
       'created_at': instance.createdAt,
       'updated_at': instance.updatedAt,
       'deleted_at': instance.deletedAt,
