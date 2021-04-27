096d9f9a8dfa344b7a2ec7cbbdf8381c89d51b9c
bellbeesdev <thebellbeesdev@gmail.com>
2021-04-27 12:17:36 +0530

    initial

---
+++ lib/models/TBBTerm.dart
@@ -0,0 +1,62 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:tbbsdk/models/TBBTaxonomy.dart';
+
+
+/// This allows the `User` class to access private members in
+/// the generated file. The value for this is *.g.dart, where
+/// the star denotes the source file name.
+part 'TBBTerm.g.dart';
+
+/// An annotation for the code generator to know that this class needs the
+/// JSON serialization logic to be generated.
+@JsonSerializable(
+    fieldRename: FieldRename.snake, nullable: true, explicitToJson: true)
+class TBBTerm {
+  String id;
+  String name;
+  String slug;
+  String icon;
+  String parent;
+  String taxonomyId;
+  TBBTaxonomy taxonomy;
+  @JsonKey(name: "sr_terms")
+  List<TBBTerm> serviceTerms;
+  @JsonKey(name: "hy_terms")
+  List<TBBTerm> hyperTerms;
+  String createdAt;
+  String updatedAt;
+  String deletedAt;
+
+  TBBTerm({
+    this.id,
+    this.name,
+    this.slug,
+    this.icon,
+    this.parent,
+    this.taxonomyId,
+    this.hyperTerms,
+    this.taxonomy,
+    this.serviceTerms,
+    this.createdAt,
+    this.updatedAt,
+    this.deletedAt,
+  });
+
+  /// A necessary factory constructor for creating a new User instance
+  /// from a map. Pass the map to the generated `_$UserFromJson()` constructor.
+  /// The constructor is named after the source class, in this case, User.
+  factory TBBTerm.fromJson(Map<String, dynamic> json) {
+    json['id'] = json['id'].toString();
+    json['parent'] = json['parent'].toString();
+    json['taxonomy_id'] = json['taxonomy_id'].toString();
+    return _$TBBTermFromJson(json);
+  }
+
+  /// `toJson` is the convention for a class to declare support for serialization
+  /// to JSON. The implementation simply calls the private, generated
+  /// helper method `_$UserToJson`.
+  Map<String, dynamic> toJson() => _$TBBTermToJson(this);
+
+  @override
+  toString() => this.toJson().toString();
+}
---
+++ lib/models/TBBTerm.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'TBBTerm.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TBBTerm _$TBBTermFromJson(Map<String, dynamic> json) {
+  return TBBTerm(
+    id: json['id'] as String,
+    name: json['name'] as String,
+    slug: json['slug'] as String,
+    icon: json['icon'] as String,
+    parent: json['parent'] as String,
+    taxonomyId: json['taxonomy_id'] as String,
+    hyperTerms: (json['hy_terms'] as List)
+        ?.map((e) =>
+            e == null ? null : TBBTerm.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    taxonomy: json['taxonomy'] == null
+        ? null
+        : TBBTaxonomy.fromJson(json['taxonomy'] as Map<String, dynamic>),
+    serviceTerms: (json['sr_terms'] as List)
+        ?.map((e) =>
+            e == null ? null : TBBTerm.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    createdAt: json['created_at'] as String,
+    updatedAt: json['updated_at'] as String,
+    deletedAt: json['deleted_at'] as String,
+  );
+}
+
+Map<String, dynamic> _$TBBTermToJson(TBBTerm instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'slug': instance.slug,
+      'icon': instance.icon,
+      'parent': instance.parent,
+      'taxonomy_id': instance.taxonomyId,
+      'taxonomy': instance.taxonomy?.toJson(),
+      'sr_terms': instance.serviceTerms?.map((e) => e?.toJson())?.toList(),
+      'hy_terms': instance.hyperTerms?.map((e) => e?.toJson())?.toList(),
+      'created_at': instance.createdAt,
+      'updated_at': instance.updatedAt,
+      'deleted_at': instance.deletedAt,
+    };
